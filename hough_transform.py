bdc7733aff5a26a19d1f3f339afa366aa3f7aa11
nyan101 <k.minchul95@gmail.com>
2021-05-23 23:11:28 +0900

    중복 선분 제거

--- hough_transform.py
+++ hough_transform.py
@@ -1,14 +1,26 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('./image2.jpg')
+img = cv2.imread('./image3.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 150, 300, apertureSize = 3) # threshold1, threshold2, apertureSize
 lines = cv2.HoughLines(edges, 1, np.pi/180, 150) # r정밀도(1), theta정밀도(1라디안), threshold
 # threshold 값이 작을수록 디테일해짐
 
+real_lines = []
+W, H, C = img.shape
 for line in lines:
     rho,theta = line[0]
+
+    for r,t in real_lines:
+        if ((r-rho)/W)**2 + ((t-theta)/np.pi)**2 < 0.01: # threshold 값을 키울수록 선이 많이 사라짐
+            break
+    else:
+        real_lines.append((rho, theta))
+
+
+for line in real_lines:
+    rho,theta = line
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a*rho
